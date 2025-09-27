ac3d8de9f749d320b58a87650f34de8f5c8d47f8
sahilsinhacodes <sinhasahil423@gmail.com>
2025-09-27 15:36:16 +0530

    day 2

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import DashboardWrapper from "./DashboardWrapper";
+import DashboardWrapper from "./dashboardWrapper";
+import React from "react";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
