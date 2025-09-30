92398b722fdcfa75f0d81de97ff2a04d122025b4
sahilsinhacodes <sinhasahil423@gmail.com>
2025-09-30 14:46:46 +0530

    day 5

---
+++ server/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: "inventosync",
+        script: "npm",
+        args: "run dev",
+        env: {
+          NODE_ENV: "development",
+          ENV_VAR1: "environment-variable",
+        },
+      },
+    ],
+  };
