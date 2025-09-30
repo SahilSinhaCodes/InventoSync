923f0ee2c6ecf3361ec2fd00523ea85885923ec5
sahilsinhacodes <sinhasahil423@gmail.com>
2025-09-30 18:03:14 +0530

    Update layout.tsx

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import DashboardWrapper from "./dashboardWrapper";
+// This is the best practice
+import DashboardWrapper from '@/app/dashboardWrapper';
 import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
